6c593476c4d851136e051f2b889de284fa406cae
Ryan Barrett <git@ryanb.org>
2023-05-18 14:08:13 -0700

    tests: make -v flag turn on logging

--- arroba/tests/__init__.py
+++ arroba/tests/__init__.py
@@ -1,1 +1,9 @@
+import logging, sys
+
+# Piggyback on unittest's -v and -q flags to show/hide logging.
+logging.basicConfig()
+if '-v' in sys.argv:
+  logging.getLogger().setLevel(logging.DEBUG)
+elif 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
+  logging.disable(logging.CRITICAL + 1)
 
