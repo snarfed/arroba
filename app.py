0907cdbedb57a48ff9489e411b43db48b6d3f1d6
Ryan Barrett <git@ryanb.org>
2023-08-04 12:43:33 -0700

    misc setup for deploying prototype PDS on App Engine
    up and serving at https://arroba-pds.appspot.com/

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 import logging
 import os
 
+import google.cloud.logging
+
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
+for logger in ('google.cloud', 'oauthlib', 'requests', 'requests_oauthlib',
+               'urllib3'):
+  logging.getLogger(logger).setLevel(logging.INFO)
 # logging.getLogger('lexrpc').setLevel(logging.INFO)
 
 from flask import Flask
@@ -14,8 +19,12 @@ import lexrpc.flask_server
 from arroba import server
 from arroba import xrpc_identity, xrpc_repo, xrpc_server, xrpc_sync
 
-
-if os.environ.get('GAE_ENV') != 'standard':
+if os.environ.get('GAE_ENV') == 'standard':
+    os.environ.setdefault('ARROBA_PASSWORD', open('pds_password').read().strip())
+    os.environ.setdefault('ARROBA_JWT', open('pds_jwt').read().strip())
+    logging_client = google.cloud.logging.Client()
+    logging_client.setup_logging(log_level=logging.DEBUG)
+else:
     os.environ.setdefault('ARROBA_PASSWORD', 'sooper-sekret')
     os.environ.setdefault('ARROBA_JWT', 'towkin')
 
--- arroba/server.py
+++ arroba/server.py
@@ -18,8 +18,7 @@ random.seed(1234567890)
 
 # XRPC server
 lexicons = []
-# TODO: vendor in lexicons
-for filename in (Path(__file__).parent.parent.parent / 'atproto/lexicons/com/atproto').glob('**/*.json'):
+for filename in (Path(__file__).parent / 'lexicons').glob('**/*.json'):
     with open(filename) as f:
         lexicons.append(json.load(f))
 
