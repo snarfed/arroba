a58c083fc7f796d7d08813ee67345641eb691d36
Ryan Barrett <git@ryanb.org>
2023-08-24 15:27:37 -0700

    subscribe.py, misc tweaks

--- subscribe.py
+++ subscribe.py
@@ -3,7 +3,7 @@
 Outputs DAG-JSON encoded payloads to stdout in JSON Lines format, one object per
 line.
 
-Usage: subscribe.py BGS_HOST [CURSOR]
+Usage: subscribe.py [BGS_HOST [CURSOR]]
 """
 import json
 import os
@@ -19,8 +19,9 @@ for filename in (Path(__file__).parent / 'arroba/lexicons').glob('**/*.json'):
         lexicons.append(json.load(f))
 
 if __name__ == '__main__':
-    client = Client(f'https://{sys.argv[1]}', lexicons)
-    assert len(sys.argv) in (2, 3)
+    assert len(sys.argv) <= 3
+    host = sys.argv[1] if len(sys.argv) >= 2 else 'bgs.bsky-sandbox.dev'
+    client = Client(f'https://{host}', lexicons)
     kwargs = {'cursor': sys.argv[2]} if len(sys.argv) == 3 else {}
     for msg in client.com.atproto.sync.subscribeRepos(**kwargs):
-        print(dag_json.encode(msg), file=sys.stdout)
+        print(dag_json.encode(msg).decode(), file=sys.stdout)
