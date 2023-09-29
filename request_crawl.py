976176a9e501ed038d56d7f3503826ea0a7df0ea
Ryan Barrett <git@ryanb.org>
2023-09-29 11:16:22 -0700

    refactor request_crawl to use lexrpc.Client, util.service_jwt

--- request_crawl.py
+++ request_crawl.py
@@ -1,37 +1,32 @@
 """Run to connect your PDS to the BGS/AppView, once you've started it running."""
+import logging
 import os
+from datetime import timedelta
 from pathlib import Path
-from datetime import datetime, timedelta
 
-import requests
+from lexrpc.client import Client
 
-from arroba import jwt_monkeypatch as jwt
+from arroba import util
 
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
 
-os.environ.setdefault('BGS_HOST', 'bgs.bsky-sandbox.dev')
-os.environ.setdefault('PDS_HOST', open('pds_host').read().strip())
-os.environ.setdefault('REPO_PRIVKEY', open('privkey.pem').read().strip())
+repo_did = os.environ.get('REPO_DID')
+if not repo_did:
+    did_docs = list(Path(__file__).parent.glob('did:plc:*.json'))
+    assert len(did_docs) == 1, f'Expected one DID doc file; got {did_docs}'
+    repo_did = did_docs[0].name.removesuffix('.json')
 
-did_docs = list(Path(__file__).parent.glob('did:plc:*.json'))
-assert len(did_docs) == 1, f'Expected one DID doc file; got {did_docs}'
-os.environ.setdefault('REPO_DID', did_docs[0].name.removesuffix('.json'))
+privkey_pem = os.environ.get('REPO_PRIVKEY') or open('privkey.pem').read().strip()
 
-
-# https://atproto.com/specs/xrpc#inter-service-authentication-temporary-specification
-# https://atproto.com/specs/cryptography
-token = jwt.encode({
-    'iss': os.environ['REPO_DID'],
-    'aud': f'did:web:{os.environ["BGS_HOST"]}',
-    'exp': int((datetime.now() + timedelta(days=999)).timestamp()),  # 😎
-}, os.environ['REPO_PRIVKEY'], algorithm='ES256K')
-
-scheme = ('http' if os.environ["BGS_HOST"].startswith('localhost')
-                 or os.environ["BGS_HOST"].startswith('127.0.0.1')
+bgs = os.environ.get('BGS_HOST', 'bgs.bsky-sandbox.dev')
+scheme = ('http' if bgs.startswith('localhost') or bgs.startswith('127.0.0.1')
           else 'https')
-url = f'{scheme}://{os.environ["BGS_HOST"]}/xrpc/com.atproto.sync.requestCrawl'
-print(f'Fetching {url}')
-resp = requests.post(url, json={'hostname': os.environ['PDS_HOST']},
-                     headers={'Authorization': f'Bearer {token}'})
-print(resp.content)
-resp.raise_for_status()
+token = util.service_jwt(host=bgs, repo_did=repo_did, privkey=privkey_pem,
+                         expiration=timedelta(days=999))
+
+client = Client(f'{scheme}://{bgs}', access_token=token)
+pds = os.environ.get('PDS_HOST') or open('pds_host').read().strip()
+client.com.atproto.sync.requestCrawl({'hostname': pds})
+
 print('OK')
