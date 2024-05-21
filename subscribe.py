9df7f449a0a74e65ef1d6dcf32edb24d10abfb26
Ryan Barrett <git@ryanb.org>
2024-05-21 15:52:21 -0700

    subscribe.py: truncate block bytes, print records in operations

--- subscribe.py
+++ subscribe.py
@@ -9,6 +9,9 @@ import json
 import os
 import sys
 
+from carbox.car import Block, read_car, write_car
+from carbox.message import read_event_pair
+import dag_cbor
 import dag_json
 from lexrpc.client import Client
 
@@ -19,5 +22,19 @@ if __name__ == '__main__':
     scheme = 'http' if host.split(':')[0] == 'localhost' else 'https'
     client = Client(f'{scheme}://{host}')
     kwargs = {'cursor': sys.argv[2]} if len(sys.argv) == 3 else {}
-    for msg in client.com.atproto.sync.subscribeRepos(**kwargs):
-        print(dag_json.encode(msg).decode(), file=sys.stdout, flush=True)
+
+    for header, payload in client.com.atproto.sync.subscribeRepos(**kwargs):
+        output = json.loads(dag_json.encode(payload).decode())
+        output['blocks'] = output['blocks']['/']['bytes'][:32] + '…'
+        print(output.get('seq'), header, output,
+              file=sys.stdout, flush=True)
+
+        roots, blocks = read_car(payload['blocks'])
+        if blocks:
+            blocks = {block.cid: block for block in blocks}
+            for op in payload.get('ops', []):
+                record = blocks[op['cid']].decoded if op['cid'] else ''
+                print('    ', op['action'], op['path'], record,
+                      file=sys.stdout, flush=True)
+
+        print()
