50d04ddf72afd9e831cc3f57c401f7b93a4f60f9
Ryan Barrett <git@ryanb.org>
2025-04-19 14:51:29 -0700

    add vis.py for visualizing MSTs
    generates DOT, then uses GraphViz to render that to PNG
    
    useful for debugging #52
    
    related: https://github.com/DavidBuchanan314/mst-test-suite/blob/main/scripts/render_testcase_html.py

---
+++ arroba/vis.py
@@ -0,0 +1,80 @@
+"""Visualizes an MST via GraphViz.
+
+Usage: python -m arroba.vis
+"""
+import json
+import os
+from pathlib import Path
+import subprocess
+
+from multiformats import CID
+
+from .mst import Leaf, MST
+from .storage import Action, Block, CommitData, CommitOp, MemoryStorage
+
+
+def render(tree):
+    """Outputs GraphViz to render an MST.
+
+    Args:
+      tree (mst.MST)
+      stream (file): where to output
+
+    Returns:
+      str: GraphViz rendering
+    """
+    out = 'digraph {\ngraph [ordering = out]\n'
+
+    def name(node):
+        if isinstance(node, MST):
+            return node.pointer.encode("base32")
+        else:
+            return f'{node.value.encode("base32")}_{node.key.replace("/", "_")}'
+
+    for entry in tree.walk():
+        if isinstance(entry, Leaf):
+            cid = entry.value
+            shape = 'box'
+            key = f'{entry.key}\\n'
+        else:
+            assert isinstance(entry, MST)
+            cid = entry.pointer
+            shape = 'circle'
+            key = ''
+
+        label = f'{key}…{cid.encode("base32")[-7:]}'
+        out += f'{name(entry)} [shape = {shape}, label = "{label}"]\n'
+
+        if isinstance(entry, MST):
+            for child in entry.get_entries():
+                out += f'{name(entry)} -> {name(child)}\n'
+
+    out += '}'
+    return out
+
+
+if __name__ == '__main__':
+    for case in json.load((Path(os.path.dirname(__file__))
+                           / 'tests/testdata/commit-proof-fixtures.json'
+                           ).open()):
+        storage = MemoryStorage()
+        tree = MST.create(storage=storage)
+        val = CID.decode(case['leafValue'])
+
+        # initial tree
+        for key in case['keys']:
+          tree = tree.add(key, val)
+
+        filename = f'vis_{case.get("comment", "").replace(" ", "_").replace("-", "_")}'
+        with open(f'{filename}_before.png', 'wb') as f:
+            subprocess.run(['dot', '-Tpng'], input=render(tree).encode(), stdout=f)
+
+        # new writes
+        for key in case['adds']:
+            tree = tree.add(key, val)
+
+        for key in case['dels']:
+            tree = tree.delete(key)
+
+        with open(f'{filename}_after.png', 'wb') as f:
+            subprocess.run(['dot', '-Tpng'], input=render(tree).encode(), stdout=f)
