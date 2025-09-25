c2db243cd5df1867e4a6431fda539d0d517abeb5
Ryan Barrett <git@ryanb.org>
2025-09-25 11:24:38 -0700

    in tests, when suppressing logs, change stream instead of level
    do this instead of setLevel() or disable() so that the log messages still get evaluated and raise the same exceptions that they would if they got emitted

--- arroba/tests/__init__.py
+++ arroba/tests/__init__.py
@@ -1,4 +1,4 @@
-import logging, sys
+import io, logging, sys
 
 # Piggyback on unittest's -v and -q flags to show/hide logging.
 logging.basicConfig()
@@ -7,4 +7,7 @@ if '-v' in sys.argv:
   logging.getLogger('PIL').setLevel(logging.INFO)
 elif 'discover' in sys.argv or '-q' in sys.argv or '--quiet' in sys.argv:
   logging.disable(logging.CRITICAL + 1)
-
+  # don't emit logs. do this instead of setLevel() or disable() so that the log
+  # messages still get evaluated and raise the same exceptions that they would if
+  # they got emitted.
+  logging.getLogger().handlers[0].setStream(io.StringIO())
